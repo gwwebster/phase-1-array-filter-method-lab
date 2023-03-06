587b743226e525d866823adb8dee64e233a3dde6
George <gwwebster23@gmail.com>
2023-03-05 18:17:44 -0600

    Array filter method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, string) {
+    let antiCaser = string.toLowerCase()
+    return array.filter(function (name) {
+        let eitherCaseName = name.toLowerCase()
+        return (eitherCaseName === antiCaser)
+    })
+}
+
+function fuzzyMatch(array, string) {
+    let stringLen = string.length
+    return array.filter(name => (name.slice(0, stringLen) === string))
+}
+
+function matchName(driverObjArray, string) {
+    return driverObjArray.filter(driver => (driver.name === string))
+}
